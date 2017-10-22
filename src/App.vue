1f2cbbbcad7b0df7f1ff01e415672886f86be767
Alp Çelik <alparslancelik@anadolu.edu.tr>
2017-10-23 00:50:53 +0300

    Proper working

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
   <div id="app">
-    <div class="container is-desktop">
+    <div class="columns">
+    <div class="column is-half is-offset-one-quarter ">
         <b-field label="Name of Coffee"><b-input v-model="name" id="name"></b-input></b-field>
         <b-field label="Region of Coffee"><b-input v-model="region" id="region"></b-input></b-field>
         <b-field label="Type of Coffee"><b-input v-model="type" id="type" ></b-input></b-field>
         <b-field label="Method of Coffee"><b-input v-model="method" id="method" ></b-input></b-field>
-        <b-field label="Description of Coffee"><b-input v-model="description" id="description"maxlength="200" type="textarea"></b-input>  </b-field>
+        <b-field label="Description of Coffee"><b-input v-model="description" id="description"maxlength="1024" type="textarea"></b-input>  </b-field>
         <div class="field"> <b-checkbox v-model="milk" id="milk">There is milk in this coffee</b-checkbox>  </div>
         <b-field label="Grinding size  of Coffee Beans"><b-input  v-model="grindingType" id="grindingType"></b-input></b-field>
-        <b-field label="Preperation time of Coffee"><b-input v-model="prepTime" id="prepTime"></b-input></b-field>
+        <b-field label="Preperation time of Coffee in Minutes"><b-input v-model="prepTime" id="prepTime"></b-input></b-field>
         <b-field> <p class="control">  <button class="button is-primary" v-on:click.prevent="sendata" > Send Coffee Recipes</button></p></b-field>
         </div>
-    <table>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-         <th>Region</th>
-        
-      </tr>
-      <tr v-for="coffee in coffees"> 
-        <td>{{coffee.id}}</td>
-        <td>{{coffee.name}}</td>
-        <td>{{coffee.region}}</td>
+        </div>
+         <div class="columns" >
+            <div class="column is-one-third" v-for="coffee in coffees">
+                <div class="card">
+                  <div class="card-content">
+                    <div class="media">
+                      <div class="media-content">
+                        <p class="title is-4">{{coffee.name}}</p>
+                        <p class="subtitle is-5">Region: {{coffee.region}} </br>
+                         Type:  {{coffee.name}} </br>
+                         With Milk: {{coffee.milk}} </br>
+                         Grinding Type: {{coffee.grindingType}} </br>
+                         Preperation Time : {{coffee.prepTime}} Minutes </br>
+                        <p class="content is-4">Description: {{coffee.description}}</p>
+                        
+                      </div>
+                    </div>
+                  </div>
+            </div>
+            </div>
+      </div>
 
-      </tr>
-    </table>
   </div>
 </template>
 
